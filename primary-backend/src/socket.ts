e4b4734779cddacde611ab24bda830a513e38398
akshxdevs <akshxdevs@gmail.com>
2025-05-04 12:46:24 +0530

    made changes in be code base

--- primary-backend/src/socket.ts
+++ primary-backend/src/socket.ts
@@ -76,7 +76,7 @@ export const setUpSocketServer  = (server:httpServer) => {
                 if (!message.value) return;
                 const parsed = JSON.parse(message.value.toString());
                 const { from, to, text, group } = parsed;
-                const payload = { from, text };
+                const payload = { from, text,to };
                 if (group) {
                     Broadcast(group,payload);
                     console.log(`Message from ${from} broadcasted to group ${group}`);
